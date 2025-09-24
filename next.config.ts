9702f35db25a944a3adbabb5bc5e3a84207eaa20
Oleksii Popov <alexejpopovua@gmail.com>
2025-09-24 17:20:04 +0200

    ci: add github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type {NextConfig} from "next";
 
+const repoName = "image-studio-demo"
+const isGithubPages = process.env.GITHUB_PAGES === "true"
+
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath: isGithubPages ? `/${repoName}` : undefined,
+  assetPrefix: isGithubPages ? `/${repoName}/` : undefined,
+  trailingSlash: true,
   experimental: {
     reactCompiler: true
   },
